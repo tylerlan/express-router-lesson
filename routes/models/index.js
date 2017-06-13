188dfc09507c83c5d6a9f9ad87a00d3a66bc945d
Namkai Fairfield <namkai.fairfield@gmail.com>
2017-06-12 23:17:17 -0700

    slight tweaks to results

--- routes/models/index.js
+++ routes/models/index.js
@@ -2,9 +2,9 @@ const models = require('express').Router();
 const all = require('./all');
 const single = require('./single');
 const cars = require('./cars');
-const findObject = require('../../utils/findObject');
+//const findObject = require('../../utils/findObject');
 
-models.param('modelId', findObject('model'));
+//models.param('modelId', findObject('model'));
 
 models.get('/', all);
 models.get('/:modelId', single);
