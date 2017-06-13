4de4028bce33490b9ca9ff33fd7ba2111e769a99
Namkai Fairfield <namkai.fairfield@gmail.com>
2017-06-12 19:02:39 -0700

    starting place

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 // Bring in our dependencies
 const app = require('express')();
-const routes = require('./routes');
 
-//  Connect all our routes to our application
-app.use('/', routes);
 
 // Turn on that server!
 app.listen(3000, () => {
