cc7d7ec8d2f97703ca75ccbeba01504017bbf901
Namkai Fairfield <namkai.fairfield@gmail.com>
2017-06-12 19:01:37 -0700

    slight refactor

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 // Bring in our dependencies
 const app = require('express')();
-const routes = require('./routes');
+const models = require('./routes/models');
+const cars = require('./routes/cars');
 
 //  Connect all our routes to our application
-app.use('/', routes);
+
+app.use('/cars', cars);
+app.use('/models', models);
+
+app.get('/', (req, res) => {
+	res.status(200).json({ message: 'Connected!' });
+});
 
 // Turn on that server!
 app.listen(3000, () => {
 	console.log('App listening on port 3000');
 });
-
